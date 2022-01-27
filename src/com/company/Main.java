e2772468586557913252a57eb490342ba4447fd0
tamanna <faria.tamanna18@gmail.com>
2022-01-26 20:17:02 -0500

    here's ur greencar ,

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,12 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
        CarBasic car = new CarBasic();
-       car.color="red";
-       car.engine="799cc";
-       car.door="2";
-       car.number="119018";
+
+       car.color="green";
+       car.number="789";
+       car.engine="544444cc";
+       car.door="3";
+       
         System.out.println(car.color);
     }
 }
