65e42d010d0e54f05ce083bdb7857f31e9403ef1
tamanna <faria.tamanna18@gmail.com>
2022-01-26 20:55:59 -0500

    these are the new changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,9 @@ public class Main {
        CarBasic car = new CarBasic();
 
        car.color="green";
-       car.number="789";
-       car.engine="544444cc";
-       car.door="3";
+       car.number="789511220";
+       car.engine="544444c00c";
+       car.door="4";
        
         System.out.println(car.color);
     }
