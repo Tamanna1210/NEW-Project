cf216167ed8a8f3b762fcd63f056830bf3ec44ce
tamanna <faria.tamanna18@gmail.com>
2022-01-26 19:56:16 -0500

    the red car is ready for trial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,10 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
        CarBasic car = new CarBasic();
+       car.color="red";
+       car.engine="799cc";
+       car.door="2";
+       car.number="119018";
+        System.out.println(car.color);
     }
 }
