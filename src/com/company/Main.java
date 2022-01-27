9f77a4beef4fd89c96066039b8ef40c512856843
tamanna <faria.tamanna18@gmail.com>
2022-01-26 21:15:01 -0500

    latest final commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,11 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
        CarBasic car = new CarBasic();
-
        car.color="green";
        car.number="789511220";
        car.engine="544444c00c";
        car.door="4";
-       
+
         System.out.println(car.color);
     }
 }
---
+++ src/com/company/headLights.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class headLights {
+    String power;
+    String color;
+    String size;
+
+}
